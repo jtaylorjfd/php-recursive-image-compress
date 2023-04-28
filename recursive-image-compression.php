6c5dea3ee107b6589a31c5e19284f310559ccc5c
JTaylor <jtaylor@jfd.co.uk>
2023-04-28 17:27:53 +0100

    formatting

--- recursive-image-compression.php
+++ recursive-image-compression.php
@@ -9,7 +9,6 @@ $it = new RecursiveDirectoryIterator($FILE_NAME);
 foreach(new RecursiveIteratorIterator($it) as $file) {
     if ($file->getExtension() == 'jpg') {
         $newFileName = preg_replace("/\s+|'|,/", '_', $file);
-        $newFileName = preg_replace('/[\(|\)]+/', '', $newFileName);  
 
         rename($file, $newFileName);
         shell_exec('magick '.$newFileName.' -strip -interlace Plane -quality 85% '.$newFileName);
@@ -19,21 +18,3 @@ foreach(new RecursiveIteratorIterator($it) as $file) {
 }
 */
 
-$FILE_NAME = 'compressed2';
-
-// Construct the iterator
-$it = new RecursiveDirectoryIterator($FILE_NAME);
-
-// Loop through files
-foreach(new RecursiveIteratorIterator($it) as $file) {
-    if ($file->getExtension() == 'jpg') {
-        if(str_contains( $file, '1400x900')){
-            $file = str_replace($FILE_NAME, '', $file);
-            echo "'img/properties".$file ."',\r\n";
-
-        }
-
-
-
-    }
-}
